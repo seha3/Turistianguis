2843259c0d1068249daf0487bbf4ee8a08d7b442
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-16 15:05:50 -0500

    made the changes to get the page to render

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
 import Gerson from './Component/button.js'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-
- class MapContainer extends Component {
+class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> yoo, sup foo ! </h1>
+        <h1> yoo! </h1>
         <Gerson />
-
-
-         <Map google={this.props.google} zoom={14}>
- 
-      <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
-
-      <InfoWindow onClose={this.onInfoWindowClose}>
-    
- </InfoWindow>
-</Map>
       </div>
 
     );
   }
 }
 
-
-
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyCrZ089AaLuQIQZMpj0xFxHH4I3Y9YCp64')
-})(MapContainer)
+export default hot(module)(App);
