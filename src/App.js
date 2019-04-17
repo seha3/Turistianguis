02b416434280305679a7f7f9853e7027933effc1
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-17 15:52:45 -0500

    created the marker

--- src/App.js
+++ src/App.js
@@ -4,25 +4,58 @@ import "./App.css";
 import Gerson from './Component/button.js'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
  
-const mapStyles = {
-  width: '50%',
-  height: '50%'
-};
+
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
   render() {
+    const mapStyles = {
+      width: '40%',
+      height: '50%'
+    };
     return (
       <Map
         google={this.props.google}
         zoom={14}
         style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+        initialCenter={{ lat: -1.2884, lng: 36.8233 }}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
-}
+
+  }
 
  
 export default GoogleApiWrapper({
