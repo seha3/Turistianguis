f4a1005947e3313a539445202d31c6e5fc0b3623
Adrian Fernandez <adrianfernandez@softbank220001003086.bbtec.net>
2019-04-19 14:52:43 -0500

    added search bar componenet and fixed excessive width

--- client/src/Component/SearchBar.js
+++ client/src/Component/SearchBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
-class Searchbar extends React.Component {
+import '../SearchBar.css'
+class SearchBar extends React.Component {
   render () {
-
-
-  }
+    return (
+    <div id="cover">
+      <form method="get" action="">
+        <div class="tb">
+          <div class="td"><input type="text" placeholder="Search" required/></div>
+          <div class="td" id="s-cover">
+            <button type="submit">
+            <div id="s-circle"></div>
+            <span></span>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )};
 }
 
-export default Searchbar;
+export default SearchBar;
