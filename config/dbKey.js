9c223e014d38966d02217e72215a0c48eca7d689
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-24 12:44:15 -0500

    Created db and connected db to backend

---
+++ config/dbKey.js
@@ -0,0 +1,5 @@
+//export our MongoDB URI(Uniform Resource Identifier)
+module.exports = {
+  mongoURI: "mongodb+srv://Fox:1ZqLhNvtsIHAX1sf@tapp-wkcbg.gcp.mongodb.net/test?retryWrites=true",
+  secretOrKey: 'secret' 
+};
